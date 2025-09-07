722cedb0c21a5aa155bc238ae0b6a95df12d89da
Fauzan <fauzdasoodais@gmail.com>
2025-09-08 01:31:07 +0300

    again

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,16 @@ import "./globals.css";
 export const metadata = {
   title: "Solo Leveling",
   description: "Improve your skills with Solo Leveling",
+  manifest: "/manifest.json",
+  themeColor: "#1f2937",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "Solo Leveling",
+  },
+  icons: {
+    apple: "/icons/apple-icon-180.png",
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -22,11 +32,6 @@ export default function RootLayout({ children }) {
       lang="en"
       className="min-h-screen overflow-x-hidden bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 "
     >
-      <head>
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icons/apple-icon-180.png" />
-        <meta name="theme-color" content="#1f2937" />
-      </head>
       <body className="text-white">{children}</body>
     </html>
   );
