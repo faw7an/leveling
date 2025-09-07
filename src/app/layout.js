84c5ce5d450b2c57766575a1146b00e3e3a3a676
Fauzan <fauzdasoodais@gmail.com>
2025-09-08 01:33:17 +0300

    trail

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
-
 export const metadata = {
   title: "Solo Leveling",
   description: "Improve your skills with Solo Leveling",
@@ -18,12 +8,18 @@ export const metadata = {
   themeColor: "#1f2937",
   appleWebApp: {
     capable: true,
-    statusBarStyle: "default",
+    statusBarStyle: "black-translucent",
     title: "Solo Leveling",
   },
   icons: {
+    icon: "/icons/manifest-icon-192.png",
     apple: "/icons/apple-icon-180.png",
   },
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    viewportFit: "cover"
+  }
 };
 
 export default function RootLayout({ children }) {
