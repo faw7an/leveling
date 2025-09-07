f2cc9fe2e0ad6c4fdecf86b5cae8dee8f924cb19
Fauzan <fauzdasoodais@gmail.com>
2025-09-08 01:43:00 +0300

    pwea

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+import withPWA from '@ducanh2912/next-pwa';
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+
+export default withPWA({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+})(nextConfig);
+
