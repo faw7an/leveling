95c3ba177dace1bc79c18507f63d6643fc22989f
Fauzan <fauzdasoodais@gmail.com>
2025-09-08 01:28:26 +0300

    link manifest

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,10 +18,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="min-h-screen overflow-x-hidden bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 ">
-      <body className="text-white" >
-        {children}
-      </body>
+    <html
+      lang="en"
+      className="min-h-screen overflow-x-hidden bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 "
+    >
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/apple-icon-180.png" />
+        <meta name="theme-color" content="#1f2937" />
+      </head>
+      <body className="text-white">{children}</body>
     </html>
   );
 }
